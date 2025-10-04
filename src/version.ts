ffa606e7f2a827961f09c2ed108b1959e551db96
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2025-10-04 01:46:14 +0100

    fix(package): Update version to 1.9.1 and remove unused dependencies

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,7 @@
-import { readPackageUpSync } from "read-package-up";
-
-export const VERSION = readPackageUpSync()!.packageJson.version
+/**
+ * The current version that you are currently using.
+ *
+ * Note to developers: This needs to explicitly be `string` so it is not typed as a "const string" that gets injected by esbuild
+ */
+// eslint-disable-next-line @typescript-eslint/no-inferrable-types
+export const VERSION: string = '[VI]{{inject}}[/VI]';
